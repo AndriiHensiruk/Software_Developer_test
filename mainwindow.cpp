fbe23e7b61a6fee9fd4c831e26313e24804d56df
AndriiHensiruk <62516941+AndriiHensiruk@users.noreply.github.com>
2021-01-07 21:00:34 +0200

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,13 @@
-#include "mainwindow.h"
+#include "MainWindow.h"
 #include "ui_mainwindow.h"
+
+#include "QPixmap"
+#include "QApplication"
+#include "QDesktopWidget"
+#include <QList>
+#include <QScreen>
+#include "QPainter"
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +21,12 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QDesktopWidget* dw = QApplication::desktop();
+    QPixmap pixmap = QPixmap::grabWindow(dw->winId(), 0, 0,
+                                         dw->width(), dw->height());
+    pixmap.save(name, "JPG", screenshot_quality);
+
+}
